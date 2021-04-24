6518a7520aecd1d1d624a8bc2f9ee2e914f193e5
MB-study <tim211413@gmail.com>
2021-04-24 16:30:08 +0800

    20210417 homework

--- app_food/src/main/java/com/applab/app_food/MainActivity.kt
+++ app_food/src/main/java/com/applab/app_food/MainActivity.kt
@@ -95,6 +95,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    // homework
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
