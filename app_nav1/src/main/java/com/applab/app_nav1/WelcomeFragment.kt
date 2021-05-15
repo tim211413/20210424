49b4416d8a32361d324f5ee0970f128fe13a9574
MB-study <tim211413@gmail.com>
2021-05-15 16:23:28 +0800

    20210515

---
+++ app_nav1/src/main/java/com/applab/app_nav1/WelcomeFragment.kt
@@ -0,0 +1,26 @@
+package com.applab.app_nav1
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_welcome.*
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("MainActivity", arguments.toString())
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text_name.text = "Hi " + arguments?.getString("username")
+
+        button_home.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
---
+++ app_sqlite/src/main/java/com/applab/app_sqlite/models/Student.kt
@@ -0,0 +1,5 @@
+package com.applab.app_sqlite.models
+
+data class Student(var id: Int, var name: String, var score: Int, var ct: Long) {
+
+}
